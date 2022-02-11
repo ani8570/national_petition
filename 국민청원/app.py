aeac8acf9d92ba3726985cd135651d9c99786d30
ani8570 <ani8570@naver.com>
2022-02-11 10:05:37 +0900

    1

--- 국민청원/app.py
+++ 국민청원/app.py
@@ -22,4 +22,4 @@ def contents(contents):
     return render_template('contents/'+contents+'.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
